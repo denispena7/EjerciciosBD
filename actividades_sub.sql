cc5cce7afbc5991ed1f9783647812bf18b57e4df
denispena7 <penadenis007@gmail.com>
2024-02-20 13:26:23 +0100

    second commit

---
+++ actividades_sub.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE actividades_subacuaticas CHARSET utf8mb4 COLLATE utf8mb4_spanish2_ci;
+USE actividades_subacuaticas;
+
+CREATE TABLE clientes (
+	idCliente INT AUTO_INCREMENT NOT NULL,
+    nombreCliente VARCHAR(45) NOT NULL,
+    apellidosCliente VARCHAR(45) NOT NULL,
+    dniCliente VARCHAR(9),
+    telefonoCliente INT NOT NULL,
+    correoElectronicoCliente VARCHAR(100) NOT NULL,
+    PRIMARY KEY (idCliente)
+);
+
+CREATE TABLE actividades (
+	idActividad INT AUTO_INCREMENT NOT NULL,
+    descripcionActividad VARCHAR(100) NOT NULL,
+    precioActividad DECIMAL(6,2) NOT NULL,
+    duracionActividad TIME NOT NULL,
+    nivelRequeridoActividad VARCHAR(45) NOT NULL,
+    PRIMARY KEY (idActividad)
+);
+
+CREATE TABLE facturas (
+	idFactura INT AUTO_INCREMENT NOT NULL,
+    ivaFactura DECIMAL(4,2) NOT NULL,
+    importeTotalFactura DECIMAL(8,2) NOT NULL,
+    fechaFactura DATE NOT NULL,
+    PRIMARY KEY (idFactura)
+);
+
+CREATE TABLE contrataciones (
+	idClienteFK INT,
+    idActividadFK INT,
+    fechaContratacionActividad DATE,
+    PRIMARY KEY (idClienteFK, idActividadFK),
+    FOREIGN KEY (idClienteFK) REFERENCES clientes (idCliente),
+    FOREIGN KEY (idActividadFK) REFERENCES actividades (idActividad)
+);
+
+CREATE TABLE generar_facturas (
+	idFacturaFK INT,
+    idActividadFK INT,
+    PRIMARY KEY (idFacturaFK, idActividadFK),
+    FOREIGN KEY (idFacturaFK) REFERENCES facturas (idFactura),
+    FOREIGN KEY (idActividadFK) REFERENCES actividades (idActividad)
+);
+
+INSERT INTO clientes VALUES (NULL, 'Mario', 'Medrano', 'Z1234567X', 605205305, 'mario@gmail.com'), 
+(NULL, 'Josué', 'Pérez', 'Z7654321X', 611235987, 'josuepz@gmail.com');
+
+SELECT * FROM clientes;
--- gestion.sql
+++ gestion.sql
@@ -33,5 +33,16 @@ INSERT INTO empleados VALUES(null, 'Nuria', 'Turner', 'Contable', 1500, 2);
 INSERT INTO empleados VALUES(null, 'Ellen', 'Smith', 'Contable', 1500, 2);
 INSERT INTO empleados VALUES(null, 'Susan', 'Ward', 'Comercial', 2000, 1);
 
+CREATE TABLE usuarios (
+    idUsuario INT AUTO_INCREMENT,
+    nombreUsuario VARCHAR(45) NOT NULL,
+    claveUsuario VARCHAR(256) NOT NULL,
+    tipoUsuario TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (idUsuario)
+);
+
+INSERT INTO usuarios VALUES (null, 'Admin','c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f', 0), 
+(null, '0e35f6e9742e074dfd62e874de3c242c6d9b64c21bf9afbaf3d11f05579b4495', 'Basic', 1);
+
 SELECT * FROM departamentos;
 SELECT * FROM empleados;
