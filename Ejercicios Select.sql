d82e3b3828d0bff9a77161f769481cbaefb6fa73
denispena7 <penadenis007@gmail.com>
2024-03-14 10:57:15 +0100

    Eigth commit

--- Ejercicios Select.sql
+++ Ejercicios Select.sql
@@ -1,13 +1,11 @@
--- Ejercicio 1: Obtener los nombres de todos los centros de trabajo de la empresa clasificados
--- por orden alfabético
-
+-- Ejercicio 1.1
 SELECT
 	nombreCentroTrabajo AS 'Centro de Trabajo'
 FROM
 	centrostrabajo
 ORDER BY 1;
 
--- Ejercicio 2: Obtener los nombres de los empleados que trabajan en el departamento 121.
+-- Ejercicio 1.2
 SELECT
 	nombreEmpleado AS 'Nombre Empleado'
 FROM
@@ -15,15 +13,14 @@ FROM
 WHERE idDepartamentoFK = 121
 ORDER BY nombreEmpleado;
 
--- Ejercicio 3: Extraer todos los datos del departamento 121.
+-- Ejercicio 1.3
 SELECT
 	*
 FROM
 	departamentos
 WHERE idDepartamento = 121;
 
--- Ejercicio 4: Obtener los nombres y salarios de los empleados con más de tres hijos por
--- orden alfabético
+-- Ejercicio 1.4
 SELECT
 	nombreEmpleado AS 'Nombre',
     salarioEmpleado AS 'Salario'
@@ -32,7 +29,7 @@ FROM
 WHERE hijosEmpleado > 3
 ORDER BY 1;
 
--- Ejercicio 5
+-- Ejercicio 1.5
 SELECT
 	comisionEmpleado AS 'Comisión',
 	idDepartamentoFK AS 'Departamento',
@@ -48,7 +45,7 @@ ASC,
 DESC;
 
 	
--- Ejercicio 6
+-- Ejercicio 1.6
 SELECT
 	nombreDepartamento AS 'Departamento'
 FROM
@@ -58,7 +55,7 @@ WHERE
 ORDER BY
 	1;
     
--- Ejercicio 7
+-- Ejercicio 1.7
 SELECT
 	DISTINCT idDepartamentoFK AS 'Departamento'
 FROM
@@ -67,7 +64,7 @@ WHERE
 	salarioEmpleado < 2000
 ORDER BY 1;
 
--- Ejercicio 8
+-- Ejercicio 1.8
 SELECT
 	DISTINCT comisionEmpleado AS 'Comisión'
 FROM
@@ -75,7 +72,7 @@ FROM
 WHERE
 	idDepartamentoFK = 110;
 
--- Ejercicio 9
+-- Ejercicio 1.9
 SELECT
 	nombreDepartamento AS 'Departamento'
 FROM
@@ -85,7 +82,7 @@ WHERE
 ORDER BY 1;
 
 
--- Ejercicio 10
+-- Ejercicio 1.10
 SELECT
 	nombreEmpleado AS 'Empleado',
     idEmpleado AS '#',
@@ -96,14 +93,14 @@ WHERE
 	idDepartamentoFK = 121
 ORDER BY 1;
 
--- Ejercicio 11
+-- Ejercicio 1.11
 SELECT
 	DISTINCT extensionEmpleado AS 'Extensión'
 FROM
 	empleados
 ORDER BY extensionEmpleado;
 
--- Ejercicio 12
+-- Ejercicio 1.12
 SELECT
 	comisionEmpleado AS 'Comisión',
     nombreEmpleado AS 'Empleado',
@@ -115,7 +112,7 @@ WHERE
 ORDER BY
 	nombreEmpleado;
 
--- Ejercicio 13
+-- Ejercicio 1.13
 SELECT
 	CONCAT(salarioEmpleado, ' €') AS 'Salario',
     nombreEmpleado AS 'Empleado'
@@ -126,6 +123,156 @@ WHERE
 ORDER BY    
 	salarioEmpleado DESC, nombreEmpleado;
 
+USE empresa;
+-- Ejercicio 1.14
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE nombreEmpleado LIKE 'MORA%, %'
+ORDER BY 1;
 
+-- Ejercicio 1.15
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE nombreEmpleado LIKE '%, VALERIANA%'
+ORDER BY 1;
 
+-- Ejercicio 1.16
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE nombreEmpleado LIKE '_______, %'
+ORDER BY 1;
 
+-- Ejercicio 1.17
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE
+	nombreEmpleado LIKE '______%, %'
+ORDER BY 1;
+
+-- Ejercicio 1.18
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE
+	nombreEmpleado LIKE '_, %'
+    OR nombreEmpleado LIKE '__, %'
+    OR nombreEmpleado LIKE '___, %'
+ORDER BY 1;
+-- WHERE nombreEmpleado NOT LIKE '____%, %';
+    
+-- Ejercicio 1.19
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE nombreEmpleado LIKE '%EZ, %__O';
+
+-- Ejercicio 1.20
+SELECT
+	nombreEmpleado AS 'Nombre Empleado',
+    salarioEmpleado AS 'Salario',
+    fechaIngresoEmpleado AS 'Fecha de ingreso'
+FROM
+	empleados
+WHERE
+	fechaIngresoEmpleado > '1988-01-01'
+    OR salarioEmpleado < 2000
+ORDER BY 3, 1;
+
+-- Ejercicio 1.21
+SELECT
+	nombreDepartamento AS 'Departamento'
+FROM
+	departamentos
+WHERE 
+	nombreDepartamento NOT LIKE 'DIRECCION %'
+	AND nombreDepartamento NOT LIKE 'SECTOR %'
+ORDER BY 1;
+
+-- Ejercicio 1.22
+SELECT
+	nombreDepartamento AS 'Nombre Departamento'
+FROM
+	departamentos
+WHERE
+	tipoDirectorDepartamento = 'F'
+    OR (tipoDirectorDepartamento = 'P' AND presupuestoDepartamento > 5)
+    OR idDepartamentoFK IS NULL
+ORDER BY 1;
+
+-- Ejercicio 2.1
+SELECT 
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE
+	extensionEmpleado IN (250, 750)
+ORDER BY 1;
+
+-- Ejercicio 2.2
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE
+	idDepartamentoFK IN (SELECT idDepartamentoFK FROM empleados WHERE nombreEmpleado = "FLOR, DOROTEA")
+ORDER BY 1;
+
+-- Ejercicio 2.3
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE
+	salarioEmpleado BETWEEN 2500 AND 3001
+ORDER BY 1;
+
+-- Ejercicio 2.4
+SELECT
+	nombreEmpleado AS 'Nombre Empleado',
+    CONCAT(salarioEmpleado, ' €') AS 'Salario'
+FROM
+	empleados
+WHERE
+	(salarioEmpleado / hijosEmpleado BETWEEN 0 AND 1200)
+    OR (salarioEmpleado/ hijosEmpleado > comisionEmpleado * 2)
+ORDER BY nombreEmpleado;
+
+-- Ejercicio 2.5
+SELECT
+	nombreEmpleado AS 'Invitados 1er día',
+    hijosEmpleado + 2 AS 'Invitaciones',
+    hijosEmpleado AS 'Regalos'
+FROM
+	empleados
+WHERE 
+	nombreEmpleado BETWEEN 'A' AND 'M'
+ORDER BY 1;
+
+-- Ejercicio 3.1
+SELECT
+	nombreEmpleado AS 'Nombre Empleado',
+    CONCAT(salarioEmpleado * 12, ' €') AS 'Salario Anual'
+FROM
+	empleados
+WHERE
+	idDepartamentoFK = 100
+ORDER BY 2 DESC;
+
+-- Ejercicio 3.2
+SELECT
+	nombreEmpleado AS 'Nombre Empleado'
+FROM
+	empleados
+WHERE
+	comisionEmpleado >= (salarioEmpleado / 2)
+ORDER BY 1;
